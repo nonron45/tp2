86ef8993b376e474bf8e12617a3777b4e8bc34a9
sad-aou <nonron4545@gmail.com>
2025-10-29 10:50:46 +0100

    fanction to return n with ","

--- String_Calculator.py
+++ String_Calculator.py
@@ -2,16 +2,7 @@ def string_Calculator1(n: str)-> int:
     if  n== "":
         return ""
 
-    if n == "1,2":
-        return 3
-    if n == "0,1":
-        return 1
-    if n == "0,0":
-        return 0
-    if n == "1,1":
-        return 2
-    if n == "2,2":
-        return 4
-    if n == "3,3":
-        return 6
+    if ',' in n:
+        a, b = n.split(',')
+        return int(a) + int(b)
     return int(n)
