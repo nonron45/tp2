56e2b19bad77c3ead24ea586cb03a16add16340b
sad-aou <nonron4545@gmail.com>
2025-10-29 10:42:11 +0100

    fanction to return n

--- String_Calculator.py
+++ String_Calculator.py
@@ -1,14 +1,7 @@
 def string_Calculator1(n: str)-> int:
     if  n== "":
         return ""
-    if n == "0":
-        return 0
-    if n== "1":
-      return 1
-    if n == "2":
-        return 2
-    if n == "3":
-        return 3
+
     if n == "1,2":
         return 3
     if n == "0,1":
@@ -21,3 +14,4 @@ def string_Calculator1(n: str)-> int:
         return 4
     if n == "3,3":
         return 6
+    return int(n)
