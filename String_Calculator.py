adf0dffc6ad758966f46a4ac4ab441c7f6849f91
sad-aou <nonron4545@gmail.com>
2025-10-29 10:38:36 +0100

    more tests

--- String_Calculator.py
+++ String_Calculator.py
@@ -9,3 +9,15 @@ def string_Calculator1(n: str)-> int:
         return 2
     if n == "3":
         return 3
+    if n == "1,2":
+        return 3
+    if n == "0,1":
+        return 1
+    if n == "0,0":
+        return 0
+    if n == "1,1":
+        return 2
+    if n == "2,2":
+        return 4
+    if n == "3,3":
+        return 6
--- test_string_Calculator.py
+++ test_string_Calculator.py
@@ -1,6 +1,6 @@
 import pytest
 from  String_Calculator import string_Calculator1
-@pytest.mark.parametrize("n, expected_result",[("",""),("0",0),("1",1),("2",2),("3",3)])
+@pytest.mark.parametrize("n, expected_result",[("",""),("0",0),("1",1),("2",2),("3",3),("1,2",3),("0,1",1),("0,0",0),("1,1",2),("2,2",4),("3,3",6)])
 def test_string_Calculator1_returns_empty(n:str, expected_result:int):
     result = string_Calculator1(n)
     assert result == expected_result
