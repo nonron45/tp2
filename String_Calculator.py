6531e5621c37827ab34cf92b6335a86d378c0086
sad-aou <nonron4545@gmail.com>
2025-10-29 10:33:25 +0100

    test empty & "0" &"1"&"2"&"3"

--- String_Calculator.py
+++ String_Calculator.py
@@ -5,3 +5,7 @@ def string_Calculator1(n: str)-> int:
         return 0
     if n== "1":
       return 1
+    if n == "2":
+        return 2
+    if n == "3":
+        return 3
--- test_string_Calculator.py
+++ test_string_Calculator.py
@@ -1,8 +1,6 @@
-from unittest import result
-
 import pytest
 from  String_Calculator import string_Calculator1
-@pytest.mark.parametrize("n, expected_result",[("",""),("0",0),("1",1)])
+@pytest.mark.parametrize("n, expected_result",[("",""),("0",0),("1",1),("2",2),("3",3)])
 def test_string_Calculator1_returns_empty(n:str, expected_result:int):
     result = string_Calculator1(n)
     assert result == expected_result
